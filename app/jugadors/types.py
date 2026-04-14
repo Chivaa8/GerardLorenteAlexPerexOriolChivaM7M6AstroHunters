fdc5f6f897ef2c218c3f5337298fa3b8d2020328
Chivaa8 <adaw23ochiva@inslaferria.cat>
2026-04-14 10:37:32 +0200

    refactor(jugadors): relación con partidas no pide en enunciado

--- app/jugadors/types.py
+++ app/jugadors/types.py
@@ -1,12 +1,14 @@
 import strawberry
-from typing import List, Optional
+from typing import List
 from app.firebase_conf import db
+
 
 @strawberry.type
 class Item:
     id: str
     nom_item: str
-    raresa: str 
+    raresa: str
+
 
 @strawberry.type
 class Jugador:
@@ -26,23 +28,12 @@ class Jugador:
 
         return llista_items
 
-    @strawberry.field
-    def partides(self) -> List[strawberry.LazyType["Partida", "app.partides.types"]]:
-        from app.partides.types import Partida
-
-        resultat = []
-        for doc in db.collection("partides").stream():
-            partida_data = doc.to_dict() or {}
-            puntuacions = doc.reference.collection("puntuacions").where("jugador_id", "==", self.id).limit(1).stream()
-            if next(puntuacions, None) is not None:
-                resultat.append(Partida.from_firestore(doc.id, partida_data))
-
-        return resultat
 
 @strawberry.input
 class RegistrarJugadorInput:
     nickname: str
-   
+
+
 @strawberry.input
 class AtorgarItemInput:
     jugador_id: str
